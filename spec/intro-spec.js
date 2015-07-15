68f2178ca376d1e8168aa7dff78022deb13f89a6
AnnaErshova <anna.ershova@gmail.com>
2015-07-15 10:34:07 -0400

    all done

--- spec/intro-spec.js
+++ spec/intro-spec.js
@@ -2,43 +2,43 @@
 describe('Intro to Jasmine and JS basics', function() {
   describe('Variables', function() {
     it('should create a variable called car with the value "Mustang" that is not assigned to the window', function() {
-      // some assignment goes here.
+      var car = "Mustang";
       expect(car).toBe('Mustang');
       expect(window.car).toBe(undefined);
     });
     it('should have a variable return undefined without explicitly assigning it to undefined', function() {
-      // something goes here.
-      expect(/* your answer goes here */).toBe(undefined);
+      var greeting;
+      expect(greeting).toBe(undefined);
     });
   });
 
   describe('Strings', function() {
     it('should use string concatention to create a string with the following variables', function() {
-      var oh = 'Oh',
-          my = 'my',
-          how = 'how',
-          i = 'I',
-          love = 'love',
-          javascript = 'JavaScript';
-      expect(/* your answer goes here */).toBe('Oh my, how I love JavaScript!!!');
+      var oh = 'Oh ',
+          my = 'my, ',
+          how = 'how ',
+          i = 'I ',
+          love = 'love ',
+          javascript = 'JavaScript!!!';
+      expect(oh.concat(my,how,i,love,javascript)).toBe('Oh my, how I love JavaScript!!!');
     });
     it('should select the right character from the string', function() {
-      expect('kreplach'/* your answer goes here */).toBe('c');
+      expect('kreplach'.substr(6,1)).toBe('c');
     });
   });
 
   describe('Numbers', function() {
     it('should correctly floor four', function() {
-      expect(4.00000000000001).toBe(4);
+      expect(Math.floor(4.00000000000001)).toBe(4);
     });
   });
 
   describe('Booleans', function() {
     it('should add two booleans together to get 1', function() {
-      expect(/* your answer goes here */).toBe(1);
+      expect(true + false).toBe(1);
     });
     it('should convert 0 into false', function() {
-      expect(/* your answer goes here */).toBe(false);
+      expect(0 == true).toBe(false);
     });
   });
 
@@ -46,6 +46,8 @@ describe('Intro to Jasmine and JS basics', function() {
     describe('For Loops', function() {
       it('should be able to use for loops to loop through an array', function() {
         var arr = [0, 0.3, 0.6, 1]
+        var i = 1
+        //arr.forEach(function(i))
         /* your for loop code starts here here */
           expect( Math.ceil(arr[i] * 3) ).toEqual(i);
         /* and ends here */
@@ -53,7 +55,7 @@ describe('Intro to Jasmine and JS basics', function() {
     });
     describe('#forEach', function() {    
       it('should be able to iterate through an array and use numbers and their indices', function() {
-        [1, 2, 3, 4, 5].forEach(function(/* needs to pass some args */){
+        [1, 2, 3, 4, 5].forEach(function(num, i){
           expect(num - 1).toEqual(i);
         });
       });
